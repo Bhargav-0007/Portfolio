fc0f39902dc50f8f890c408d6a42b1404ffff09f
Bhargav <pvb.8394@gmail.com>
2026-05-06 09:31:33 -0500

    Make Projects grid responsive for 2 cards

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -32,7 +32,7 @@ function Projects() {
           </p>
         </div>
 
-        <div className="grid md:grid-cols-3 gap-8">
+        <div className="grid sm:grid-cols-2 gap-8 max-w-4xl mx-auto">
           {projects.map((project, i) => (
             <div
               key={i}
